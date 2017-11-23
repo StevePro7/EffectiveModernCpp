3a57c4c26eee6d0f6e30f20e84d9975282072154
StevePro7 <steveprozune@hotmail.com>
2017-11-23 19:07:56 +0000

    Update files

--- Chap01_Deducing Types/01-Item01/Src/Item.cpp
+++ Chap01_Deducing Types/01-Item01/Src/Item.cpp
@@ -1,6 +1,34 @@
+// Pg.17
+// Function Arguments
+
 #pragma once
+
+// someFunc is a function;
+// type is void(int, double)
+void someFunc( int, double )
+{
+}
+
+// f1 param passed by value
+template<typename T>
+void f1( T param )
+{
+}
+
+// f2 param passed by reference
+template<typename T>
+void f2( T& param )
+{
+}
 
 int main()
 {
+    // param deduced as ptr-to-func
+    // type is void(*)(int, double)
+    f1( someFunc );
+
+    // param deduced as ref-to-func
+    // type is void(&)(int, double)
+    f2( someFunc );
     return 0;
 }
