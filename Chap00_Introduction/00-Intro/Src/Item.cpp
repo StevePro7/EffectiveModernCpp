c860aab540ecfbf2a69eeb463e7fadb3c2d74d9b
StevePro7 <steveprozune@hotmail.com>
2017-11-23 16:57:59 +0000

    Update item

--- Chap00_Introduction/00-Intro/Src/Item.cpp
+++ Chap00_Introduction/00-Intro/Src/Item.cpp
@@ -1,6 +1,66 @@
 #pragma once
 
+// C/C++ Lanugage | C++ Lanugage standard
+#include <utility>
+
+class Widget
+{
+public:
+    Widget() {}
+
+    // If not defined will get "attempting to reference a deleted function"
+    Widget( Widget& lhs ) {}
+
+    // rhs is an lvalue though it has an rvalue reference type
+    Widget( Widget&& rhs ) {}
+};
+
+// These are C++ source code ellipses
+template<typename... Ts>
+void processVals( const Ts&... params )
+{
+}
+
+// someFunc parameter w is passed by value
+void someFunc( Widget w )
+{
+}
+
+// Declarations
+extern int x;                   // object declaration
+class xWidget;                  // class declaration
+bool xFunc( const xWidget& w ); // function declaration
+enum class Color;               // scoped enum declaration
+
+// Definitions
+int x;                          // object definition
+class yWidget                   // class definition
+{
+};
+
+bool yFunc( const yWidget& w )  // function definition
+{
+    return true;
+}
+
+enum class Color                // scoped enum definition
+{
+    Yellow, Red, Blue
+};
+
 int main()
 {
+    // wid is some Widget
+    Widget wid;
+
+    // In this call to someFunc, w i s a copy of wid that's created via copy construction
+    someFunc( wid );
+    
+    // In this call to someFunc, w i s a copy of wid that's created via move construction
+    someFunc( std::move( wid ) );
+
+
+    
+
     return 0;
 }
