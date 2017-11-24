225bad52447d939b75910513f6eb73996d70e763
StevePro7 <steveprozune@hotmail.com>
2017-11-24 11:07:07 +0000

    Add more code

--- Chap01_Deducing Types/02-Item02/Src/Item.cpp
+++ Chap01_Deducing Types/02-Item02/Src/Item.cpp
@@ -26,6 +26,17 @@ void someFunc( int, double )
 {
 }
 
+// template with parameter declaration equivalent to x declaration
+template<typename T>
+void f1( T param )
+{
+}
+
+template<typename T>
+void f2( std::initializer_list<T> initList )
+{
+}
+
 int main()
 {
     auto x = 27;            // case 3   x neither ptr nor ref
@@ -48,16 +59,24 @@ int main()
     auto func1 = someFunc;                  // func1 type   void(*)(int, double)
     auto& func2 = someFunc;                 // func2 type   void(&)(int, double)
 
+    // all four syntaxes but one result: an int with value 27
     int x1 = 27;        // C++98
     int x2( x1 );       // C++98
     int x3 = { 27 };    // C++11
     int x4{ 27 };       // C++11
 
-    // all compile
+    // all compile but don't have the same meaning as above
     auto y1 = 27;       // C++98            int                             value = 27
     auto y2( y1 );      // C++98            int                             value = 27
     auto y3 = { 27 };   // C++11            std::initializer_list<int>      value = { 27 }
     auto y4{ y3 };      // C++11            std::initializer_list<int>      value = { 27 }
 
+    // error! can't deduce T for std::initializer_list<T>
+    //auto x5 = { 1, 2, 3.0 };
+
+    auto x6 = { 11, 23, 9 };                // type    std::initializer_list<int>
+    //f1( { 11, 13, 9 } );                  // error! can't deduce type for T
+    f2( { 11, 13, 9 } );                    // T deduced as int
+                                            // initList type    std::initializer_list<int>
     return 0;
 }
