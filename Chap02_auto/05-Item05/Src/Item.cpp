3bc1d875d7794c6514349ab2db735d6c87aebd2b
StevePro7 <steveprozune@hotmail.com>
2017-12-17 13:55:02 +0000

    Update Chap05

---
+++ Chap02_auto/05-Item05/Src/Item.cpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+
+
+// algorithm to dwim (do what I mean)
+template<typename IT>
+void dwim( IT b, IT e )
+{
+    while( b != e )
+    {
+        //typename std::iterator_tratis<IT>::value_type currValue = *b;
+        auto currValue = = *b;
+    }
+}
+
+
+class Widget
+{
+public:
+    Widget Widget::operator<( Widget &other ) 
+    {
+        return data < other.data ? *this : other;
+    }
+
+private:
+    int data;
+};
+
+
+int main()
+{
+    int x1;         // potentially uninitialized
+    //auto x2;        // error! initializer required
+    auto x3 = 0;    // fine x's value is well-defined
+
+    //auto derefUPLess =
+    //    []( const std::unique_ptr<Widget>& p1,
+    //        const std::unique_ptr<Widget>& p2 )
+    //{
+    //    return *p1 < *p2;
+    //};
+
+    auto derefUPLess =
+        []( const auto& p1,
+            const auto& p2 )
+    {
+        return *p1 < *p2;
+    };
+
+    //std::function<bool( const std::unique_ptr<Widget>&, const std::unique_ptr<Widget>& )>
+    //    derefUPLess = []( const std::unique_ptr<Widget>& p1, const std::unique_ptr<Widget>& p2 )
+    //{
+    //    return *p1 < *p2; 
+    //};
+
+    int m[5] = { 1, 2, 3, 4, 5 };
+    int sum = 0;
+    for( const auto& p : m )
+    {
+        sum += p;
+    }
+
+    return 0;
+}
