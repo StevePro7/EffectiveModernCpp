5351738584d20bb57c8a3c0d143cf49fc0c6da5f
StevePro7 <steveprozune@hotmail.com>
2017-12-19 16:52:00 +0000

    Update code

--- Chap03_Moving_to_Modern_C++/11-Item11/Src/Data01.h
+++ Chap03_Moving_to_Modern_C++/11-Item11/Src/Data01.h
@@ -11,21 +11,4 @@ public:
     bool isLucky( char ) = delete;
     bool isLucky( bool ) = delete;
     bool isLucky( double ) = delete;
-
-    template<typename T>
-    void processPointer( T* ptr )
-    {
-    }
-
-    template<>
-    void processPointer<void>( void* ) = delete;
-
-    template<>
-    void processPointer<char>( char* ) = delete;
-
-    template<>
-    void processPointer<const void>( const void* ) = delete;
-
-    template<>
-    void processPointer<const char>( const char* ) = delete;
 };
