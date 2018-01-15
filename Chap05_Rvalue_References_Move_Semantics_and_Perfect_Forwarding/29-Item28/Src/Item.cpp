7e2efaafe9d733100840fe22e1159259d02321f7
StevePro7 <steveprozune@hotmail.com>
2018-01-15 19:39:44 +0000

    Update Item29

--- Chap05_Rvalue_References_Move_Semantics_and_Perfect_Forwarding/29-Item28/Src/Item.cpp
+++ Chap05_Rvalue_References_Move_Semantics_and_Perfect_Forwarding/29-Item28/Src/Item.cpp
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <array>
+#include <vector>
+
+class Widget {};
+
 int main()
 {
+    std::vector<Widget> vw1;
+
+    // move vw1 into vw2.
+    // Runs in constant time
+    // Only ptr in vw1 and vw2 are modified
+    auto vw2 = std::move( vw1 );
+
+
+    std::array<Widget, 10000> aw1;
+
+    // move aw1 into aw2.
+    // Runs in constant time
+    // aw1 are moved into aw2
+    auto aw2 = std::move( aw1 );
+
     return 0;
 }
