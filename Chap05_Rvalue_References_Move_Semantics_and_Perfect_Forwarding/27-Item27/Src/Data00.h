c8258164b38f724f0d81c6d02b1713aa4f53b11f
StevePro7 <steveprozune@hotmail.com>
2018-01-15 19:20:30 +0000

    Update Item27

---
+++ Chap05_Rvalue_References_Move_Semantics_and_Perfect_Forwarding/27-Item27/Src/Data00.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <assert.h>
+#include <string>
+
+class Person00
+{
+public:
+    // Replaces T&& ctor
+    explicit Person00( std::string n ) : name( std::move( n ) )
+    {
+    }
+
+    explicit Person00( int idx ) : name( nameFromIdx( idx ) )
+    {
+    }
+
+private:
+    std::string nameFromIdx( int idx ) { return "name";  }
+    std::string name;
+};
+
+
+class Person01
+{
+public:
+    template<typename T, typename = typename std::enable_if<age==18>::type>
+    explicit Person01( T&& n )
+    {
+    }
+
+private:
+    int age;
+};
+
+
+class Person02
+{
+public:
+    template<typename T, typename = typename std::enable_if<!std::is_same<Person02, typename std::decay<T>::type>::value>::type>
+    explicit Person02( T&& n )
+    {
+    }
+};
+
+
+class Person03
+{
+public:
+    template<typename T, typename = typename std::enable_if<!std::is_base_of<Person03, typename std::decay<T>::type>::value>::type>
+    explicit Person03( T&& n )
+    {
+    }
+};
+
+
+// C++14
+class Person14
+{
+public:
+    template<typename T, typename = std::enable_if_t<!std::is_base_of<Person14, std::decay_t<T>>::value>>
+    explicit Person14( T&& n )
+    {
+    }
+};
+
+
+class Person05
+{
+public:
+    template<typename T, typename = std::enable_if_t<
+        !std::is_base_of<Person05, std::decay_t<T>>::value 
+        &&
+        !std::is_integral<std::remove_reference_t<T>>::value>>
+        explicit Person05( T&& n ) : name( std::forward<T>( n ) )
+    {
+        // ctor for std::string and args convertible to std::string
+        static_assert(std::is_constructible<std::string, T>::value, "Parameter n can't be used to construct a std::string");
+    }
+
+    // ctor for integral arguments
+    explicit Person05( int idx ) : name( nameFromIdx( idx ) )
+    {
+    }
+
+private:
+    std::string nameFromIdx( int idx ) { return "name"; }
+    std::string name;
+};
+
+
+class SpecialPerson : public Person02
+{
+public:
+    // copy ctor; calls base class forwarding ctor!
+    SpecialPerson( const SpecialPerson& rhs ) : Person02( rhs )
+    {
+    }
+
+    // move ctor; calls base class forwarding ctor!
+    SpecialPerson( const SpecialPerson&& rhs ) : Person02( std::move( rhs ) )
+    {
+    }
+
+};
--- Chap05_Rvalue_References_Move_Semantics_and_Perfect_Forwarding/27-Item27/Src/Item.cpp
+++ Chap05_Rvalue_References_Move_Semantics_and_Perfect_Forwarding/27-Item27/Src/Item.cpp
@@ -1,5 +1,51 @@
 #pragma once
 
+#include <set>
+#include <string>
+#include "Data00.h"
+
+// global data structure
+std::multiset<std::string> names;
+
+// make log entry and add name to data structure
+//template<typename T>
+//void logAndAdd( T&& nane )
+//{
+//    auto now = std::chrono::system_clock::now();
+//    //log( now, "logAndAdd" );
+//    names.emplace( std::forward<T>( name ) );
+//}
+
+
+template<typename T>
+void logAndAdd( T&& name )
+{
+    // not quite correct.
+    logAndAddImpl( name, std::is_integral<T>() );
+
+    logAndAddImpl( std::forward<T>( name ), std::is_integral<typename std::remove_reference<T>::type() );
+}
+
+// non-integral argument: add it to global data structure
+template<typename T>
+void logAndAddImpl( T&& name, std::false_type )
+{
+    auto now = std::chrono::system_clock::now();
+    //log( now, "logAndAdd" );
+    names.emplace( std::forward<T>( name ) );
+}
+
+std::string nameFromIdx( int idx )
+{
+    return "name";
+}
+// integral argument: look up name and call logAndAdd with it
+void logAndAddImpl( int idx, std::true_type )
+{
+    //logAndAddImpl( nameFromIdx( idx ) );
+}
+
+
 int main()
 {
     return 0;
