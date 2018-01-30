eb58691883a4e1ebb941c4e484ab64666195e9a7
StevePro7 <steveprozune@hotmail.com>
2018-01-30 21:05:58 +0000

    Update item

--- Chap07_The_Concurrency_API/35-Item35/Src/Item.cpp
+++ Chap07_The_Concurrency_API/35-Item35/Src/Item.cpp
@@ -1,6 +1,28 @@
 #pragma once
 
+#include <thread>
+#include <future>
+
+//int doAsyncWork()
+//{
+//    return 0;
+//}
+
+// see Item14 for noexcept
+int doAsyncWork() noexcept
+{
+    return 0;
+}
+
 int main()
 {
+    // throws if no more threads are available
+    std::thread t( doAsyncWork );
+    t.join();
+
+
+    // onus of thread management is on implementer of the Standard Library
+    auto future = std::async( doAsyncWork );
+
     return 0;
 }
